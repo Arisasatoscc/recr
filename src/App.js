d58b80733f3d26d28a17f3211fd6a746e0d77cdf
Arisasatoscc <sarisa@sisco-consulting.co.jp>
2024-11-22 10:33:49 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  fetch("https://cu33x54xhe.execute-api.ap-northeast-1.amazonaws.com/dev/")
+    .then((res) => res.json()) // JSON形式に変換
+    .then((json) => setData(json))
+    .catch(() => alert("error")); // エラー発生時にアラート
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
